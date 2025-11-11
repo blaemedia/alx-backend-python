c0fd43ef1c9f9eda27d44e5aced4a3f7deb080c5
Emmanuel <blaemedia26@gmail.com>
2025-11-11 04:19:04 +0100

    Create serializers to define the many to many relationships

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .serializers import Conversation,Message
+from rest_framework import viewsets
 
 
 # Create your views here.
 def chats(request):
-    return HttpResponse("List of chats will be displayed here.")    
+    return HttpResponse("List of chats will be displayed here.")   
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = Conversation
+
+class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
+    serializer_class = Message
