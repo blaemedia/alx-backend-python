80b529f70d0f5627ed5fc714294f96af6592829f
Emmanuel <blaemedia26@gmail.com>
2025-11-11 05:37:24 +0100

    Build api endpoints with views

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -42,6 +42,15 @@ INSTALLED_APPS = [
     'chats'
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
